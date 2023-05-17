1d5ce98b478fdbaf6ae7cf70587ccf53683aa17d
Ka8Kaa <karlobilanovic@gmail.com>
2023-05-17 18:17:39 +0200

    Kod koji dohvaca ucenike iz datoteke te ih sortira sa novim ucenicima te ih upisuje u datoteku

--- MojPrviVSProgram.cpp
+++ MojPrviVSProgram.cpp
@@ -9,23 +9,49 @@ using namespace std;
 int main()
 {
 	fstream datoteka;
+	int izbor=-1;
+	int tracker = 0;
 	string line;
-	int counter = 0;
+	int counter=0;
 	datoteka.open("imenik.txt", ios::in);
 	while (getline(datoteka, line))
 		counter++;
-
-	string* imeprezime = new string[n];
-	for (int i = 0; i < n; i++)
+	datoteka.close();
+	string *imeprezime = new string[1000];
+	datoteka.open("imenik.txt", ios::in);
+	for (int i = 0; i < counter; i++)
 	{
-		getline(cin, imeprezime[i]);
+		getline(datoteka, imeprezime[i]);
 	}
-	sort(imeprezime, imeprezime + n);
-	for (int i = 0; i < n; i++)
+	datoteka.close();
+	while (izbor != 0)
+	{
+		cin >> izbor;
+		cin.ignore();
+		if (izbor == 1)
+		{
+			getline(cin, imeprezime[counter]);
+			counter++;
+		}
+	}
+	sort(imeprezime, imeprezime + counter);
+	datoteka.open("imenik.txt", ios::out | ios::trunc);
+	for (int i = 0; i < counter; i++)
 	{
 		datoteka << imeprezime[i] << endl;
 	}
 	datoteka.close();
+	datoteka.open("imenik.txt", ios::in);
+	for (int i = 0; i < counter; i++)
+	{
+		getline(datoteka, imeprezime[i]);
+	}
+	datoteka.close();
+	for (int i = 0; i < counter; i++)
+	{
+		cout << imeprezime[i] << endl;
+	}
+
 	return 0;
 }
 
